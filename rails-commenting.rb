684da56f79b3e65a3f8a0b5dc556fbd548e453b8
Echo5Mike <1raul.marquez@gmail.com>
2022-01-21 15:37:57 -0800

    worked on rails commenting

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) The class BlogPost get its inherits from the applicationcontroller
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) @post is getting assigned to the BlogPost.all, therefore when you "view" @post you will see all the blog post.
     @posts = BlogPost.all
   end
 
   def show
-    # ---3)
+    # ---3) at this @post gets assigned a single blog post with via id params
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) def is the start of making a method the "new" the name of that method
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5)@post gets to BlogPost.create and takes in blog_post_params as a arugment.  Seems to be that this is the start of creating a new post
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -33,14 +33,14 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  # ---6)
+  # ---6)we are calling our def "edit" and will be used as method to edit our blog post
   def edit
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)@post is assigned to blog via id and it is being updated in the database with the .update method.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) redirect_to is going to "redirect" the user to the blog post path and it is getting pased the arugment of @post.  It seems to be seving as a catch all to the destroy method if something didn't work properly.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) private add restriction to the params/method that you do not want you user to have access to.  Everything under private will have those restrictions. 
   private
   def blog_post_params
-    # ---10)
+    # ---10)It is my understanding that .require is requireing that the blog post contain the two permited keys.
     params.require(:blog_post).permit(:title, :content)
   end
 end
